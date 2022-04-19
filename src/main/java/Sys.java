b73887857ab8d65fab6e01ae2146b94cbf51d88b
InnaOs <R_musavarov@mail.ru>
2022-04-19 22:21:21 +0300

    hw8

---
+++ src/main/java/Sys.java
@@ -0,0 +1,14 @@
+public class Sys {
+    String pod;
+
+    public Sys() {
+    }
+
+    public String getPod() {
+        return pod;
+    }
+
+    public void setPod(String pod) {
+        this.pod = pod;
+    }
+}
